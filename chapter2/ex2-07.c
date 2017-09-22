89aecb95c7ca8c8dafb908f4e6f09c1d8c3309d0
Bram Lobbens <lobbensbram@gmail.com>
2017-09-22 19:37:31 +0000

    fix incorrect variable in invert

--- chapter2/ex2-07.c
+++ chapter2/ex2-07.c
@@ -17,9 +17,7 @@ int main()
     return 0;
 }
 
-/* invert: return x with the n bits that begin at the position p inverted, 
-leaving the others unchanged. */
 unsigned invert(unsigned x, int p, int n)
 {
-    return x + (x >> (p + 1 - n)) ^ ~(~0 << i);
+    return x + (x >> (p + 1 - n)) ^ ~(~0 << n);
 }
