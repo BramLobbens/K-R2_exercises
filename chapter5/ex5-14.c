13ecc334b117aa44d5ae2ec906f9e2056003b627
Bram Lobbens <lobbensbram@gmail.com>
2018-02-17 22:25:28 +0100

    add reverse switch

--- chapter5/ex5-14.c
+++ chapter5/ex5-14.c
@@ -1,6 +1,6 @@
 /* K&R2 exercise 5-14, page 121 */
 /*
- * WIP
+ * WIP - -r works only on -n
  */
 
 #include <stdio.h>
@@ -14,7 +14,7 @@ int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
 void _qsort(void *lineptr[], int left, int right,
 	   int (*comp)(void *, void *));
-int numcmp(char *, char *);
+int numcmp(char *, char *, int rev);
 
 /* sort input lines */
 int main(int argc, char *argv[])
@@ -41,12 +41,12 @@ int main(int argc, char *argv[])
 	}
     }
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-	if (!reverse) {
+	if (reverse) {
 	    _qsort((void **) lineptr, 0, nlines - 1,
 		   (int (*)(void *, void *))(numeric ? numcmp : strcmp));
 	}
 	else {
-	    _qsort((void **) lineptr, nlines - 1, 0,
+	    _qsort((void **) lineptr, 0, nlines - 1,
 		   (int (*)(void *, void *))(numeric ? numcmp : strcmp));
 	}
 	writelines(lineptr, nlines);
@@ -64,25 +64,35 @@ void _qsort(void *v[], int left, int right, int (*comp)(void *, void *))
     int i, last;
     void swap(void *v[], int, int);
     
+    /*if ((rev && left <= right) || (!rev && left >= right))*/
     if (left >= right)
 	return; /* do nothing if array contains fewer than two elements */
+    
     swap(v, left, (left + right) / 2);
     last = left;
+    
     for (i = left + 1; i <= right; i++)
 	if ((*comp)(v[i], v[left]) < 0)
 	    swap(v, ++last, i);
     swap(v, left, last);
+    
     _qsort(v, left, last - 1, comp);
     _qsort(v, last + 1, right, comp);
 }
 
 /* numcmp: compare s1 and s2 numerically */
-int numcmp(char *s1, char *s2)
+int numcmp(char *s1, char *s2, int rev)
 {
     double v1, v2;
 
-    v1 = atof(s1);
-    v2 = atof(s2);
+    if (!rev) {
+	v1 = atof(s1);
+	v2 = atof(s2);
+    }
+    else {
+	v1 = atof(s2);
+	v2 = atof(s1);
+    }
     if (v1 < v2)
 	return -1;
     else if (v1 > v2)
@@ -113,12 +123,10 @@ int readlines(char *lineptr[], int maxlines)
     nlines = 0;
     while ((len = getline(line, MAXLEN)) > 0)
     {
-	if (nlines >= maxlines || (p = alloc(len)) == NULL)
-	{
+	if (nlines >= maxlines || (p = alloc(len)) == NULL) {
 	    return -1;
 	}
-	else
-	{
+	else {
 	    line[len-1] = '\0';
 	    strcpy(p, line);
 	    lineptr[nlines++] = p;
@@ -141,8 +149,7 @@ int getline(char s[], int lim)
 
     for (i = 0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i)
 	s[i] = c;
-    if (c == '\n')
-    {
+    if (c == '\n') {
 	s[i] = c;
 	++i;
     }
@@ -156,8 +163,7 @@ static char *allocp = allocbuf; /* next free position */
 
 char *alloc(int n) /* return pointer to n characters */
 {
-    if (allocbuf + ALLOCSIZE - allocp >= n)
-    {
+    if (allocbuf + ALLOCSIZE - allocp >= n) {
         allocp += n;
         return allocp - n; /* old p */
     }
