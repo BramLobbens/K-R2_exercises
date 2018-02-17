831288582dc962229c6eb9f81e62032fd1d6228d
Bram Lobbens <lobbensbram@gmail.com>
2018-02-17 17:07:56 +0100

    fix to correct test cases

--- chapter5/ex5-10.c
+++ chapter5/ex5-10.c
@@ -1,16 +1,16 @@
 /* K&R2 exercise 5-10, page 118 */
 /**
- * WIP
+ * Usage: use '\*' as input instead of '*' for multiplication in bash
  */
 
 #include <stdio.h>
 #include <stdlib.h> /* strtol() */
 
-#define MAXOP 100 /* max size of operand or operator */
 #define MAXVAL 100 /* maximum depth of the val stack */
 
 int sp = 0; /* next free stack position */
-double val[MAXVAL]; /* value stack */
+int val[MAXVAL]; /* value stack */
+int *pval = val;
 
 void push(long int l)
 {
@@ -26,50 +26,54 @@ long int pop(void)
 	return val[--sp];
     else {
 	printf("error: stack empty\n");
-	return 0.0;
+	return 0;
     }
 }
 
 int main(int argc, char *argv[])
 {
     long int op2;
-    
+
     while (--argc) {
-	*++argv;
-     	switch (**argv) {
-	case '0':
-	case '1':
-	case '2':
-	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
-	    push(strtol(*argv, NULL, 10));
-	    break;
-	case '+':
-	    push(pop() + pop());
-	    break;
-	case '*':
-	    push(pop() * pop());
-	    break;
-	case '-':
-	    op2 = pop();
-	    push(pop() - op2);
-	    break;
-	case '/':
-	    op2 = pop();
-	    if (op2 != 0)
-		push(pop() / op2);
-	    else
-		printf("error: zero divisor\n");
-	    break;
-	default:
-	    printf("error: unknown command %s\n", *argv);
-	    break;
+	
+	/* push only two numbers on stack */
+	if (pval - 2 > 0) {
+	    *++argv;
+	    switch (**argv) {
+	    case '0':
+	    case '1':
+	    case '2':
+	    case '3':
+	    case '4':
+	    case '5':
+	    case '6':
+	    case '7':
+	    case '8':
+	    case '9':
+		push(strtol(*argv, NULL, 10));
+		break;
+	    case '+':
+		push(pop() + pop());
+		break;
+	    case '*':
+		push(pop() * pop());
+		break;
+	    case '-':
+		op2 = pop();
+		push(pop() - op2);
+		break;
+	    case '/':
+		op2 = pop();
+		if (op2 != 0)
+		    push(pop() / op2);
+		else
+		    printf("error: zero divisor\n");
+		break;
+	    default:
+		printf("error: unknown command %s\n", *argv);
+		break;
 	    }
+	}
     }
     printf("%ld\n", pop());
     return 0;
